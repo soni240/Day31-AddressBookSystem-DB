40323a9bbfd584bc7901b3951d461d0fbb6d3cf3
soni240 <s.fegade21@gmail.com>
2022-07-23 16:25:50 +0530

    [Gunvanti]Add:insert contact in addressbook

---
+++ SQLQuery3.sql
@@ -0,0 +1,27 @@
+
+------UC1-------
+CREATE DATABASE ADDRESS_BOOK_SERVICE;
+USE ADDRESSBOOK;
+
+------UC2------
+----CREATING TABLE-----
+CREATE TABLE ADDRESS_BOOK_SERVICE
+(
+FirstName char(25),
+LastName char(25),
+City char(25),
+StateName char(25),
+Zip int,
+Phone varchar(25),
+Email varchar(25)
+);
+ SELECT * FROM ADDRESS_BOOK_SERVICE;
+
+-----UC3-----
+ ----INSERT CONTACT IN ADDRESSBOOK------
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Priya','P','Pune','Maharashtra','412109','845784','P@gmail.com');
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Ansh','Patil','Mumbai','Maharashtra','560085','454645','ansh@gmail.com');
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Aryansh','Patil','Pune','Maharashtra','8375','763784','aryansh@gmail.com');
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('snehal','sonwane','nashik','Maharashtra','574895','09384903','snaehal@gmail.com');
+INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Priyanka','sonawane','hyderabad','Telangana','455454','09384903','priyanka@gmail.com');
+SELECT * FROM ADDRESS_BOOK_SERVICE;
